e5b7ba4a4c3b047be7a36daeb3f39ca7f7bc744e
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-21 14:40:29 +0100

    refactor to map and childElementCount

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,17 @@
 //Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item. Вийде 'У списку 3 категорії.'.
 
-const categoriesRef = document.querySelector('#categories').children;
-const categoriesQuantity = categoriesRef.length
-console.log(`У списку ${categoriesQuantity} категорії`);
+const categoriesRef = document.querySelector('#categories');
+const categoriesCount = categoriesRef.childElementCount
+console.log(`У списку ${categoriesCount} категорії`);
 
 //Для кожного елемента li.item в списку ul#categories, знайде і виведе в консоль текст заголовка елемента (тега h2) і кількість елементів в категорії (всіх вкладених в нього елементів li).
 
 
-
-for (const category of categoriesRef) {
+[...categoriesRef.children].map(category => {
 	const categoryTitleRef = category.querySelector('h2');
 	const categoryTitle = categoryTitleRef.textContent;
 	console.log(`Категорія: ${categoryTitle}`);
 
-	const elementsQuantity = category.querySelector('ul').children.length;
+	const elementsQuantity = category.querySelector('ul').childElementCount;
 	console.log(`Кількість елементів: ${elementsQuantity}`);
-}
+})
